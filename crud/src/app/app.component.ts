9453e3313ea3bb135cfe1d6e76fae2bbe0ab9927
nayla <naylapaixao@gmail.com>
2019-06-17 17:53:00 -0300

    Starting App with angular

---
+++ crud/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {HttpClientModule} from "@angular/common/http";
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CRUD Universidade';
+}
+
+export class User {
+  id: number;
+  name: string;
+  
+}
