1471544ed6f5ced2b71658ca83420162433816f4
nayla <naylapaixao@gmail.com>
2019-06-21 12:29:48 -0300

    Teacher put working

--- crud/src/app/teacher/teacher-update/teacher-update.component.ts
+++ crud/src/app/teacher/teacher-update/teacher-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, NgForm, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../service/api.service';
+import { Teacher } from 'src/model/teacher';
 
 @Component({
   selector: 'app-teacher-update',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherUpdateComponent implements OnInit {
 
-  constructor() { }
+  teacherForm: FormGroup;
+  id: number;
+  name: '';
+  lastName = '';
+  phd = false;
+  isLoadingResults = false;
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.getTeacher(this.route.snapshot.params.id);
+    this.teacherForm = this.formBuilder.group({
+      name : [null, Validators.required],
+      lastName : [null, Validators.required],
+      phd : [null, Validators.required]
+    });
+  }
+
+  getTeacher(id) {
+    this.api.getTeacher(id).subscribe(data => {
+      this.id = data.id;
+      this.teacherForm.setValue({
+        name: data.name,
+        lastName: data.lastName,
+        phd: data.phd
+      });
+    });
+  }
+
+  putTeacher(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.putTeacher(this.id, form)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/teacher/' + this.id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
   }
 
 }
