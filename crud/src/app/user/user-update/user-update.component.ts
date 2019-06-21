6540bb15ee037225e928c758629c549e4d2418fe
Nayla Paixao <naylapaixao@gmail.com>
2019-06-21 00:09:08 -0300

    Put User working

--- crud/src/app/user/user-update/user-update.component.ts
+++ crud/src/app/user/user-update/user-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-user-update',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-update.component.css']
 })
 export class UserUpdateComponent implements OnInit {
+    userForm: FormGroup;
+    id: number;
+    name: '';
+    lastName = '';
+    profile = '';
+    isLoadingResults = false;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.getUser(this.route.snapshot.params.id);
+      this.userForm = this.formBuilder.group({
+        name : [null, Validators.required],
+        lastName : [null, Validators.required],
+        profile : [null, Validators.required]
+      });
+  }
+
+  getUser(id) {
+    this.api.getUser(id).subscribe(data => {
+      this.id = data.id;
+      this.userForm.setValue({
+        name: data.name,
+        lastName: data.lastName,
+        profile: data.profile
+      });
+    });
+  }
+
+  putUser(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.putUser(this.id, form)
+      .subscribe(res => {
+          this.isLoadingResults = false;
+          this.router.navigate(['/user/' + this.id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
   }
 
 }
